a53ea444ef433c8f1d4fc6e0a4bf755fb35c29ae
punkkk <punkrus18@gmail.com>
2019-05-28 20:55:38 +0300

    initial state concept
    add: starting state with some kind of a story
    add: bind story to a game's scene object

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Scripting;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] State state;
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "I'm piece of shit";
+        textComponent.text = state.getStory();
     }
 
     // Update is called once per frame
