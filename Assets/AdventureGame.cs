7af0976a3e36e65364d84188738cbdc28e57e5ba
punkkk <punkrus18@gmail.com>
2019-05-28 20:21:34 +0300

    Praparations to a game
    add: main game class

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "I'm piece of shit";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
